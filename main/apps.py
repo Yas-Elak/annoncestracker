83a96d17a87efeed843b1fbcc3247cb565e67300
Elyaz <elalaoui87@gmail.com>
2019-09-17 21:51:42 +0200

    ipn

--- main/apps.py
+++ main/apps.py
@@ -5,11 +5,6 @@ from django.apps.config import AppConfig
 class MainConfig(AppConfig):
     name = 'main'
 
-
-
-class GrapMainConfig(AppConfig):
-    name = 'main'
-    verbose_name = 'main main' # this name will display in the Admin. You may translate this value if you want using ugettex_lazy
-
     def ready(self):
         import main.signals.handlers
+
