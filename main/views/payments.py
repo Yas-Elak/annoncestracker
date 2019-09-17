df3a883b414790478d8fb0336bf092eb26a98b2f
Elyaz <elalaoui87@gmail.com>
2019-09-17 22:57:53 +0200

    ipn

--- main/views/payments.py
+++ main/views/payments.py
@@ -19,22 +19,4 @@ def process_payment(request):
     return render(request, "main/donepayment.html")
 
 
-def paypal_ipn(request):
-    if request.method == 'POST':
-        print("ipppppnnnnnnnnn")
-    return HttpResponse('<h1>Page was found</h1>')
 
-#
-# class IPNHandler1(webapp.RequestHandler):
-#     def post(self):
-#         parameters = None
-#         # Check payment is completed, not Pending or Failed.
-#         if self.request.get('payment_status') == 'Completed':
-#             if self.request.POST:
-#                 parameters = self.request.POST.copy()
-#             if self.request.GET:
-#                 parameters = self.request.GET.copy()
-#
-#
-#             if __name__ == '__main__':
-#                 print("IPNHandler1 is being run as main().")
