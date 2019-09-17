00d97e973a7d5d11c4b4b7df153c686bd98288a7
Elyaz <elalaoui87@gmail.com>
2019-09-17 19:23:21 +0200

    paypal

--- main/views/payments.py
+++ main/views/payments.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 
 
 def cancel_payment(request):
-
+    print(request)
+    print("i'm here")
     return render(request, "main/cancelpayment.html")
 
 
 def process_payment(request):
-
+    print(request)
+    print("i'm here")
     return render(request, "main/donepayment.html")
 
