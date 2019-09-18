cb6aa71cb4048e7a184f31f2cf4c6c60964ae098
Elyaz <elalaoui87@gmail.com>
2019-09-18 19:38:32 +0200

    correction on query

--- main/signals.py
+++ main/signals.py
@@ -10,29 +10,26 @@ from main.models import UserContact, UserOrder
 
 def payment_notification(sender, **kwargs):
     ipn_obj = sender
+    user = User.objects.get(id=int(ipn_obj.custom))
     if ipn_obj.payment_status == ST_PP_COMPLETED:
         if ipn_obj.receiver_email != "receiver_email@example.com":
             # Not a valid payment
             return
-        order_exist = UserOrder.objects.filter(user=int(ipn_obj.custom), pending=1).exist()
+        order_exist = UserOrder.objects.filter(user__id=user.pk, pending=1).exist()
 
         if order_exist:
-            order = UserOrder.objects.filter(user=int(ipn_obj.custom), pending=1).order_by('-created').first().update(
+            order = UserOrder.objects.filter(user__id=user.pk, pending=1).order_by('-created').first().update(
                 paypal_payer_id=ipn_obj.payer_id, paypal_order_id=ipn_obj.txn_id, product=ipn_obj.item_name, payed=1,
                 pending=0, cancelled=0)
         else:
-            order = UserOrder(user=int(ipn_obj.custom), paypal_payer_id=ipn_obj.payer_id,
+            order = UserOrder.objects.create(user__id=user, paypal_payer_id=ipn_obj.payer_id,
                               paypal_order_id=ipn_obj.txn_id,
                               product=ipn_obj.item_name, payed=1, pending=0, cancelled=0)
-            order.save()
 
         if ipn_obj.custom == "Gold Subscription":
-            UserContact.objects.get(user=int(ipn_obj.custom)).update(normal_user=0, premium_user=0,
-                                                                     super_premium_user=1)
+            UserContact.objects.get(user__id=user.pk).update(normal_user=0, premium_user=0, super_premium_user=1)
         else:
-            UserContact.objects.get(user=int(ipn_obj.custom)).update(normal_user=0, premium_user=0,
-                                                                     super_premium_user=1)
-        user = User.objects.get(id=int(ipn_obj.custom))
+            UserContact.objects.get(user__id=user.pk).update(normal_user=0, premium_user=0, super_premium_user=1)
         mail_subject = f'Trackannonces thank you for your order.'
         message = render_to_string('main/email/contact_email.html', {
             'subscription': ipn_obj.item_name,
--- main/views/payments.py
+++ main/views/payments.py
@@ -13,9 +13,8 @@ def cancel_payment(request):
 @csrf_exempt
 def process_payment(request):
     current_user = auth.get_user(request)
-    pending = UserOrder.objects.filter(user=current_user.pk, pending=1).exists()
+    pending = UserOrder.objects.filter(user__id=current_user.pk, pending=1).exists()
     if not pending:
-        order = UserOrder(user=current_user.pk)
-        order.save()
+        order = UserOrder.objects.create(user_id=current_user.pk)
     return render(request, "main/donepayment.html")
 
