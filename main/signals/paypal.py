6235f838993fcccbb3151418d65e69ec205d91f8
Elyaz <elalaoui87@gmail.com>
2019-09-18 07:26:45 +0200

    urlib python3

--- main/signals/paypal.py
+++ main/signals/paypal.py
@@ -1,6 +1,7 @@
 import urllib
 
 from annoncestracker import settings
+import urllib.request as ur
 import urllib.parse
 
 
@@ -14,14 +15,14 @@ class Verify(object):
         post['cmd'] = '_notify-synch'
         post['tx'] = tx
         post['at'] = settings.PAYPAL_PDT_TOKEN
-        self.response = urllib.request.urlopen(settings.PAYPAL_PDT_URL, urllib.parse.urlencode(post)).read()
+        self.response = ur.urlopen(settings.PAYPAL_PDT_URL, urllib.parse.urlencode(post)).read()
         lines = self.response.split('\n')
         self.result = lines[0].strip()
         self.results = dict()
         for line in lines[1:]:  # skip first line
             linesplit = line.split('=', 2)
             if len(linesplit) == 2:
-                self.results[linesplit[0].strip()] = urllib.parse.unquote(linesplit[1].strip())
+                self.results[linesplit[0].strip()] = ur.unquote(linesplit[1].strip())
 
     def success(self):
         return self.result == 'SUCCESS' and self.results['payment_status'] == 'Completed'
