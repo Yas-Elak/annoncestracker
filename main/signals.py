8b2eeefd5962c6a9da13294d562d534f5b705076
Elyaz <elalaoui87@gmail.com>
2019-09-18 12:51:46 +0200

    ipn

--- main/signals.py
+++ main/signals.py
@@ -1,8 +1,12 @@
 from paypal.standard.ipn.signals import valid_ipn_received
 
+from main.models import UserContact
+
 
 def payment_notification(sender, **kwargs):
     ipn_obj = sender
+    # user_contact = UserContact.objects.get(user__id=1)
+    UserContact.objects.filter(id=1).update(sms=1)
     print("iiiiiiiiiiipppppppppppnnnnnnnnnnnnn")
 
 
