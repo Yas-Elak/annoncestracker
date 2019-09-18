0ec8dec8e0a10d13e02edc8cc538374e0b1dc218
Elyaz <elalaoui87@gmail.com>
2019-09-18 13:46:48 +0200

    ipn

--- main/signals.py
+++ main/signals.py
@@ -9,18 +9,18 @@ def payment_notification(sender, **kwargs):
     ipn_obj = sender
     # if ipn_obj.payment_status == ST_PP_COMPLETED:
     # user_contact = UserContact.objects.get(user__id=1)
-    UserContact.objects.filter(id=1).update(sms=0)
+    # UserContact.objects.filter(id=1).update(sms=0)
     # print(type(ipn_obj))
     # print(ipn_obj)
     # print(ipn_obj.item_namex)
     print("+++++++++++++++++++++++++")
-    print(ipn_obj.receiver_email)
+    # print(ipn_obj.receiver_email)
     # print(ipn_obj.mc_gross)
     print("+++++++++++++++++++++++++")
 
     # print(ipn_obj.payment_date)
     # print(ipn_obj.payment_status)
-    print(ipn_obj.mc_custom)
+    # print(ipn_obj.mc_custom)
 
 
 valid_ipn_received.connect(payment_notification)
