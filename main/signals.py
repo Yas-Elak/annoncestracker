7fe01351368f3f81a51d423c571c85b391da2ef3
Elyaz <elalaoui87@gmail.com>
2019-09-18 20:33:21 +0200

    correction on query

--- main/signals.py
+++ main/signals.py
@@ -22,21 +22,26 @@ def payment_notification(sender, **kwargs):
 
         if order_exist:
             print("ok the order exist")
-            order = UserOrder.objects.filter(user__id=user.pk, pending=1)
-            order.update(paypal_payer_id=ipn_obj.payer_id, paypal_order_id=ipn_obj.txn_id, product=ipn_obj.item_name,
-                         payed=1, pending=0, cancelled=0)
+            order = UserOrder.objects.filter(user__id=user.pk, pending=1).order_by('-created').first()
+            order.paypal_payer_id=ipn_obj.payer_id
+            order.paypal_order_id=ipn_obj.txn_id
+            order.product=ipn_obj.item_name
+            order.payed=1
+            order.pending=0
+            order.ancelled=0
+            order.save()
         else:
             print("well the order did'nt exist")
             order = UserOrder.objects.create(user=user, paypal_payer_id=ipn_obj.payer_id,
-                                             paypal_order_id=ipn_obj.txn_id, product=ipn_obj.item_name, payed=1,
-                                             pending=0, cancelled=0)
+                              paypal_order_id=ipn_obj.txn_id,
+                              product=ipn_obj.item_name, payed=1, pending=0, cancelled=0)
 
         if ipn_obj.item_name == "Gold Subscription":
             print("maintenant je get le item name, ici c'est gold")
-            UserContact.objects.filter(user__id=user.pk).update(normal_user=0, premium_user=0, super_premium_user=1)
+            UserContact.objects.get(user__id=user.pk).update(normal_user=0, premium_user=0, super_premium_user=1)
         else:
             print("maintenant je get le item name, ici c'est argent")
-            UserContact.objects.filter(user__id=user.pk).update(normal_user=0, premium_user=0, super_premium_user=1)
+            UserContact.objects.get(user__id=user.pk).update(normal_user=0, premium_user=0, super_premium_user=1)
         print("c'est pas tout ça mais on envoie l'email")
         mail_subject = f'Trackannonces thank you for your order.'
         message = render_to_string('main/email/contact_email.html', {
