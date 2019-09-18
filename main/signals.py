fde52fd5041c311611becd00a5b6c1cfaadac431
Elyaz <elalaoui87@gmail.com>
2019-09-18 15:56:27 +0200

    ipn

--- main/signals.py
+++ main/signals.py
@@ -7,13 +7,6 @@ from main.models import UserContact
 
 def payment_notification(sender, **kwargs):
     ipn_obj = sender
-    from pprint import pprint
-    # if ipn_obj.payment_status == ST_PP_COMPLETED:
-    # user_contact = UserContact.objects.get(user__id=1)
-    # UserContact.objects.filter(id=1).update(sms=0)
-    # print(type(ipn_obj))
-    # print(ipn_obj)
-    # print(ipn_obj.item_namex)
     print("i tried a connection")
     if ipn_obj.payment_status == ST_PP_COMPLETED:
         print("COMPLETED")
@@ -21,17 +14,7 @@ def payment_notification(sender, **kwargs):
         print(ipn_obj.payer_id)
         print(ipn_obj.txn_id)
     print("+++++++++++++++++++++++++")
-    #interessnt
-    # payment_status string
-    # created_at datetime datetime
-    # custom string
-    # item_name string
-    # payer_id string
-    # txn_id
-    #
 
-    # print(ipn_obj.payment_date)
-    # print(ipn_obj.payment_status)
 
 
 valid_ipn_received.connect(payment_notification)
