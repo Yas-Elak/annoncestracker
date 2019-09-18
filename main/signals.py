b35af9e3150a0e3a557051148bbbbe828bf6f351
Elyaz <elalaoui87@gmail.com>
2019-09-18 18:11:45 +0200

    ipn

--- main/signals.py
+++ main/signals.py
@@ -1,21 +1,47 @@
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 from paypal.standard.ipn.signals import valid_ipn_received
 from django.shortcuts import get_object_or_404
 from paypal.standard.models import ST_PP_COMPLETED
 
-from main.models import UserContact
+from main.models import UserContact, UserOrder
 
 
 def payment_notification(sender, **kwargs):
     ipn_obj = sender
-    print("i tried a connection")
     if ipn_obj.payment_status == ST_PP_COMPLETED:
-        print("COMPLETED")
-        print(ipn_obj.custom)
-        print(ipn_obj.payer_id)
-        print(ipn_obj.txn_id)
-    print("+++++++++++++++++++++++++")
+        if ipn_obj.receiver_email != "receiver_email@example.com":
+            # Not a valid payment
+            return
+        order_exist = UserOrder.objects.filter(user=int(ipn_obj.custom), pending=1).exist()
 
+        if order_exist:
+            order = UserOrder.objects.filter(user=int(ipn_obj.custom), pending=1).order_by('-created').first().update(
+                paypal_payer_id=ipn_obj.payer_id, paypal_order_id=ipn_obj.txn_id, product=ipn_obj.item_name, payed=1,
+                pending=0, cancelled=0)
+        else:
+            order = UserOrder(user=int(ipn_obj.custom), paypal_payer_id=ipn_obj.payer_id,
+                              paypal_order_id=ipn_obj.txn_id,
+                              product=ipn_obj.item_name, payed=1, pending=0, cancelled=0)
+            order.save()
+
+        if ipn_obj.custom == "Gold Subscription":
+            UserContact.objects.get(user=int(ipn_obj.custom)).update(normal_user=0, premium_user=0,
+                                                                     super_premium_user=1)
+        else:
+            UserContact.objects.get(user=int(ipn_obj.custom)).update(normal_user=0, premium_user=0,
+                                                                     super_premium_user=1)
+        user = User.objects.get(id=int(ipn_obj.custom))
+        mail_subject = f'Trackannonces thank you for your order.'
+        message = render_to_string('main/email/contact_email.html', {
+            'subscription': ipn_obj.item_name,
+        })
+        to_email = user.email
+        email = EmailMessage(
+            mail_subject, message, to=[to_email]
+        )
+        email.send()
 
 
 valid_ipn_received.connect(payment_notification)
-
