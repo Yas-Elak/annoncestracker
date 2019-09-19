57dde51c98dad6d498b5cadf7db70e8588977bba
Elyaz <elalaoui87@gmail.com>
2019-09-19 11:37:55 +0200

    password reset

--- annoncestracker/urls.py
+++ annoncestracker/urls.py
@@ -17,13 +17,14 @@ from django.conf.urls import url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 from main.views import dashboard, tracking, user_auth, alerts, homepage, user, payments, contact, upgrade
 
 urlpatterns = [
     # path('', include('main.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    # path('accounts/', include('django.contrib.auth.urls')),
     path('i18n/', include('django.conf.urls.i18n')),  # Make sure this is present
 ]
 #
@@ -42,7 +43,8 @@ urlpatterns += i18n_patterns(
     path("tracking/pause/", tracking.update_alerte_pause, name="pause_alerte"),
     path("tracking/delete/", tracking.delete, name="delete_alerte_info"),
     path("tracking/delete_all/", tracking.delete_all_alerts, name="delete_all_alerts"),
-    path("tracking/delete_all/<tracker_id>", tracking.delete_all_alerts_of_tracker, name="delete_all_alerts_of_tracker"),
+    path("tracking/delete_all/<tracker_id>", tracking.delete_all_alerts_of_tracker,
+         name="delete_all_alerts_of_tracker"),
 
     path("alerts/<tracker_id>/", alerts.index, name="alerts"),
     path("alerts/delete/<alert_id>", alerts.delete, name="delete_alerte"),
@@ -71,8 +73,32 @@ urlpatterns += i18n_patterns(
     # upgrade
     path("upgrade/", upgrade.index, name="upgrade"),
 
+    # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
+    # ask for the email
+    path('password_change/',
+         auth_views.PasswordResetView.as_view(template_name='main/registration/password_change.html',
+                                              html_email_template_name='main/email/password_reset_email.html'),
+         name='password_reset'),
 
+    # the email is sent and in the maintime we go here
+    path('password_reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='main/registration/password_reset_done.html'),
+         name='password_reset_done'),
 
+    # then the link in the email is clicked and we go here
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name="main/registration/password_reset_confirm.html"),
+         name='password_reset_confirm'),
+
+    # then if ok we go tothe confirmation page
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='main/registration/password_reset_complete.html'),
+         name='password_reset_complete'),
+
+    #then we say we can log
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+         name='password_reset_complete'),
     # to delete
 
 )
