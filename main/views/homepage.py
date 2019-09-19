67ef537496fb13e18c6bf87074147d8984759a52
Elyaz <elalaoui87@gmail.com>
2019-09-19 13:14:50 +0200

    custom error

--- main/views/homepage.py
+++ main/views/homepage.py
@@ -1,9 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def faq(request):
 
-    return render(request, "main/faq.html")
+    # return render(request, "main/faq.html")
+    return HttpResponse(status=500)
 
 
 def index(request):
