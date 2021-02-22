ed5cac4da9239f3c726b93da7a5491ef870bfa55
Yas-Elak <elalaoui87@gmail.com>
2021-02-22 21:03:16 +0100

    mobile urls and view

--- main/models.py
+++ main/models.py
@@ -35,6 +35,7 @@ class UserContact(models.Model):
     tlg_code = models.CharField(max_length=4, default=random_string)
     tlg_bot = models.BooleanField(default=0)
     tlg_chat_id = models.CharField(max_length=255, default="")
+    key_api_mobile = models.CharField(max_length=20, null=True)
 
     def __str__(self):
         return f"phone : {self.phone_number} / Email 1 : {self.email_one} / Email 2 : {self.email_two}"
--- main/views/mobile_connect.py
+++ main/views/mobile_connect.py
@@ -1,5 +1,13 @@
 from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+import random, string
+from ..models import UserContact, Alert
+from django.contrib import auth
+from django.utils.translation import gettext as _
+from django.core import serializers
 
+from ..constantes import *
 
 def jsontest(request):
     data = {
@@ -8,4 +16,83 @@ def jsontest(request):
         'is_active': True,
         'count': 28
     }
+    return JsonResponse(data)
+
+@login_required
+def apiKeyIndex(request):
+
+    current_user = auth.get_user(request)
+    user_contact = UserContact.objects.get(user__id=current_user.id)
+
+    key = user_contact.key_api_mobile
+
+    if not key:
+        key = _("Vous n'avez pas encore de clé.")
+
+    return render(request, "main/mobile-app-key.html", {"key_api": key})
+
+
+def generateKey(request):
+    current_user = auth.get_user(request)
+    user_contact = UserContact.objects.get(user__id=current_user.id)
+
+    #does the same key exist somewhere else?
+    key = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(10))
+
+    #add the user_contact id like that the key will always be unique
+    key += str(user_contact.id)
+
+    user_contact.key_api_mobile = key
+    user_contact.save()
+    return redirect('api_key_index')
+
+
+def trackersJson(request, api_key):
+
+    user_contact = UserContact.objects.get(key_api_mobile=api_key)
+    print(user_contact.user.id)
+
+    if user_contact.super_premium_user:
+        all_alerts = Alert.objects.filter(user__id=user_contact.user.id, activated=True).order_by('-alert_time')[:MAX_ARCHIVES_GOLD]
+    elif user_contact.premium_user:
+        all_alerts = Alert.objects.filter(user__id=user_contact.user.id, activated=True).order_by('-alert_time')[:MAX_ARCHIVES_ARGENT]
+    elif user_contact.normal_user:
+        all_alerts = Alert.objects.filter(user__id=user_contact.user.id, activated=True).order_by('-alert_time')[:MAX_ARCHIVES_NORMAL]
+
+    data = list(all_alerts.values())
+
+    #serialized_obj = serializers.serialize('json', [all_alerts, ])
+
+    return JsonResponse(data, safe=False)
+
+def alertsJson(request, api_key, tracker_id):
+
+    user_contact = UserContact.objects.get(key_api_mobile=api_key)
+    print(user_contact.user.id)
+
+    if user_contact.super_premium_user:
+        all_alerts = Alert.objects.filter(user__id=user_contact.user.id, activated=True).order_by('-alert_time')[:MAX_ARCHIVES_GOLD]
+    elif user_contact.premium_user:
+        all_alerts = Alert.objects.filter(user__id=user_contact.user.id, activated=True).order_by('-alert_time')[:MAX_ARCHIVES_ARGENT]
+    elif user_contact.normal_user:
+        all_alerts = Alert.objects.filter(user__id=user_contact.user.id, activated=True).order_by('-alert_time')[:MAX_ARCHIVES_NORMAL]
+
+    data = list(all_alerts.values())
+
+    #serialized_obj = serializers.serialize('json', [all_alerts, ])
+
+    return JsonResponse(data, safe=False)
+
+
+def keyVerification(request, api_key):
+
+    if UserContact.objects.filter(key_api_mobile=api_key).exists():
+        data = {
+            'key_verification': True
+        }
+    else:
+        data = {
+            'key_verification': False
+        }
+
     return JsonResponse(data)
